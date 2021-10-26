5ad1d263c1114d479a808a24d3ef73c5a7871486
crud0626 <sisofiy6048@gmail.com>
2021-10-26 19:36:06 +0900

    Add to create item function

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ function initGame() {
 
     timerSpan.innerText = `00:${playTime}`;
 
+    // Test
+    createItem();
+
     startClock(timerSpan);
     countSpan.innerText = counter;
 
@@ -79,8 +82,24 @@ function resumeGame() {
     startClock(timerSpan);
 }
 
+// Success
+// test결과 여유있게 x는 ~90vw, y는 ~30vh정도까지가 스크롤이 생기지 않아 난수를 제한할 수 있도록 했다.
+function createItem() {
+    let item = 10;
+    let itemsElem = [];
 
+    for (let i = 0; i < item; i++) {
+        let bugX = Math.random() * (90 - 0) + 0;
+        let bugY = Math.random() * (30 - 0) + 0;
 
+        let carrotX = Math.random() * (90 - 0) + 0;
+        let carrotY = Math.random() * (30 - 0) + 0;
+
+        itemsElem.push(`<div style="transform: translate(${bugX}vw, ${bugY}vh);" class="item"><img src="./img/bug.png" alt="bug"></div>`);
+        itemsElem.push(`<div style="transform: translate(${carrotX}vw,${carrotY}vh);" class="item"><img src="./img/carrot.png" alt="carrot"></div>`);
+    }
+    itemsElem.forEach((elem) => section.innerHTML += elem);
+}
 
 
 
